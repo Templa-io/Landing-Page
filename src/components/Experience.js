3ddcfd687217f00ce22f026cb622afabfbc086f4
Templa-io <humphreywritter@gmail.com>
2022-05-11 02:47:59 +0300

    init

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,22 +5,24 @@ const Experience = () => {
     <>
       <section className="grid">
           <h3>We Build Experiences</h3>
+          
           <p>A minimalist approach is the way to design a website</p>
+          <hr/>
           <div className="grid-items">
             <div>
               <h4>Design</h4>
               <p>with an emphasis on typography, white space, and mobile-optimized design, your website will look absolutely breathtaking</p>
-             
+             <button>LEARN MORE</button>
           </div>
        <div>
               <h4>Content</h4>
               <p>Our team will teach you the art of writing audience-focused content that will help you achieve the success you truly deserve</p>
-             
+              <button>LEARN MORE</button>
           </div>
           <div>
               <h4>Strategy</h4>
               <p>We help creative entrepreneurs build their digital business  by focusing on three key elements of a success online platform.</p>
-              
+              <button>LEARN MORE</button>
           </div>
           </div>
       </section>
--- src/components/ShowCase.js
+++ src/components/ShowCase.js
@@ -3,11 +3,12 @@ import React from 'react'
 const Showcase = () => {
   return (
     <>
-      <section className="showcase showcase-expand-lg w-100">
+      <section className="showcase">
         <div className="overlay">
           <h1>We Crush
              Minimal 
              Designs.</h1>
+           
           <p>MONOCHROME is a creative agency based in Chicago.We developed the Genesis Framework and build 
             mobile-optimized themes for WordPress
           </p>
